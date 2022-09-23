4f148d6fdc3cbbf5fb62662bd4b8f2f3dd139a0e
Adrian Fijałkowski <adrian.fijalkowski@htdevelopers.com>
2022-09-23 09:53:39 +0200

    axios interceptor removed

--- lib/config/axiosInstance.ts
+++ lib/config/axiosInstance.ts
@@ -1,9 +1,5 @@
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 
 const api = axios.create({});
-
-api.interceptors.response.use((response: AxiosResponse) => {
-  return response;
-});
 
 export default api;
